9f0b0a546687a1981fa8f365f21414f6d06255fb
Alexakhoun <khounalexa@gmail.com>
2024-09-02 01:01:52 +0700

    initial code

---
+++ src/main/java/com/corporate/project/laza_api/LazaApiApplication.java
@@ -0,0 +1,13 @@
+package com.corporate.project.laza_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazaApiApplication.class, args);
+	}
+
+}
